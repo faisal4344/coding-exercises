998b92adde6ed67f94b139704cc26bb06d83856b
faisal4344 <fayselfereja1997@gmail.com>
2025-05-06 11:40:25 +0300

    first test

--- demo.py
+++ demo.py
@@ -4,3 +4,10 @@ y = 3.14        # Float
 print(type(x))  # Output: <class 'int'>
 print(type(y))  # Output: <class 'float'>
 print(x)  
+# this is test
+a = 10          # Integer
+b = 3.14        # Float
+
+print(type(a))  # Output: <class 'int'>
+print(type(b))  # Output: <class 'float'>
+print(a) 
